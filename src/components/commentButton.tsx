33b7529c5b79fcf39b424a328f754b006302373e
StudioGuma <purpleeyedguma@aol.com>
2025-05-11 00:15:56 -0400

    removed redundant outer function in useEffect

--- src/components/commentButton.tsx
+++ src/components/commentButton.tsx
@@ -1,12 +1,24 @@
+import {ObjectId} from "mongodb";
 import {useState} from "react";
 import {createComment} from "@/src/data/posts";
 
-export default function CommentButton() {
+interface Props {
+	userId: ObjectId | string,
+	postId: ObjectId | string
+}
+
+export default function CommentButton(props: Props) {
 	const [box, setBox] = useState(false);
 
 	return (
 	<div>
+		<button onClick={() => setBox(!box)}>Comment</button>
 
+		{box &&
+		<div>
+
+		</div>
+		}
 	</div>
 	);
 }
