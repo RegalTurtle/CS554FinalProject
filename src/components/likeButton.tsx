8e0e9c0bbda56e536553f52a8c74a44653c4b032
StudioGuma <purpleeyedguma@aol.com>
2025-05-12 20:19:21 -0400

    changed the user ID check for like and comment buttons to be in the components themselves

--- src/components/likeButton.tsx
+++ src/components/likeButton.tsx
@@ -1,30 +1,56 @@
 import {ObjectId} from "mongodb";
 import {useState, useEffect} from "react";
 import {checkIfLiked, likePost} from "@/src/data/posts";
+import { usePathname } from 'next/navigation';
+import type { SessionPayload } from '@/src/lib/session';
 
 interface Props {
-	userId: ObjectId | string,
 	postId: ObjectId | string
 }
 
 export default function LikeButton(props: Props) {
 	const [isLiked, setIsLiked] = useState(false);
 
+	const pathname = usePathname();
+	const [session, setSession] = useState<SessionPayload | undefined | null>(null);
+
+	let userId = undefined;
+	if (session?.userId)
+		userId = session.userId;
+
+	useEffect(() => {
+		async function fetchData() {
+			const response = await fetch(`/api/session`);
+			const data = await response.json();
+			let { session } = data;
+			setSession(session);
+		}
+
+		fetchData();
+    	}, [pathname]);
+	
 	useEffect(() =>
 	{
-		const checkIfLikedFunc = async (): Promise<boolean> =>
-			await checkIfLiked(props.userId, props.postId);
+		if (userId) {
+			const checkIfLikedFunc = async (): Promise<boolean> =>
+				await checkIfLiked(userId, props.postId);
 		
-		checkIfLikedFunc().then((result: boolean) =>
-		setIsLiked(result));
+			checkIfLikedFunc().then((result: boolean) =>
+			setIsLiked(result));
+		}
+		
 	});
 
 	return (
+	<>
+	{userId &&
 	<button onClick={async () => {
-		await likePost(props.userId, props.postId);
+		await likePost(userId, props.postId);
 		setIsLiked(!isLiked);
 	}}>
 		{isLiked ? "♥ Unlike" : "♥ Like"}
 	</button>
+	}
+	</>
 	);
 }
